cead76f7533dd104554d0460f65c086ef79a09ed
KariRomero <karinaromero.1993@gmail.com>
2023-07-13 11:10:28 -0300

    fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
-import { createStore } from 'redux';
+import { createStore,applyMiddleware,compose } from 'redux';
 import reducer from './reducer';
+import thunkMiddleware  from 'redux-thunk';
+
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    composeEnhacer(applyMiddleware(thunkMiddleware))
+);
+
+export default store;
